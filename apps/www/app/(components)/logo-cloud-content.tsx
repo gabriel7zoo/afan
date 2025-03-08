17cdb79b48cdcd4555c38f3c7ccc0691b0411c89
GabrielDaAlien <105457393+gabriel7zoo@users.noreply.github.com>
2025-03-08 13:38:22 +0100

    Update logo-cloud-content.tsx

--- apps/www/app/(components)/logo-cloud-content.tsx
+++ apps/www/app/(components)/logo-cloud-content.tsx
@@ -20,7 +20,7 @@ const logos = [
 export function DesktopLogoCloud() {
   return (
     <div className="hidden md:flex w-full flex-col items-center">
-      <span className={cn("font-mono text-sm md:text-md text-white/50 text-center opacity-0 animate-fade-in-up [animation-delay:1s]")}>
+      <span className="font-mono text-sm md:text-md text-white/50 text-center opacity-0 animate-fade-in-up [animation-delay:1s]">
         Powering
       </span>
 
@@ -29,10 +29,10 @@ export function DesktopLogoCloud() {
           {logos.map((logo, idx) => (
             <div
               key={String(idx)}
-              className="relative w-[229px] h-[25px] opacity-0 animate-fade-in-up"
+              className="relative w-[100px] h-[25px] opacity-0 animate-fade-in-up"
               style={{ "--animation-delay": `calc(1.1s + .12s * ${idx})` } as CSSProperties}
             >
-              <Image src={logo.url} alt={logo.name} width={229} height={25} />
+              <Image src={logo.url} alt={logo.name} width={100} height={25} />
             </div>
           ))}
         </div>
@@ -58,8 +58,8 @@ export const MobileLogoCloud = () => {
             .map((_, index) => (
               <div key={index} className="flex shrink-0 animate-logo-cloud flex-row justify-around gap-6">
                 {logos.map((logo, key) => (
-                  <div key={String(key)} className="relative w-[229px] h-[25px]">
-                    <Image src={logo.url} alt={logo.name} width={229} height={25} />
+                  <div key={String(key)} className="relative w-[100px] h-[25px]">
+                    <Image src={logo.url} alt={logo.name} width={100} height={25} />
                   </div>
                 ))}
               </div>
